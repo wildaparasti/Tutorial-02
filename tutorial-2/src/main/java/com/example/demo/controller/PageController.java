7ee21a44fd8c865219bab44e3d5746e3feffc420
wilda.paras <wilda.paras>
2018-09-18 14:41:25 +0700

    challenge

--- tutorial-2/src/main/java/com/example/demo/controller/PageController.java
+++ tutorial-2/src/main/java/com/example/demo/controller/PageController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
 	@RequestMapping("/viral")
-	public String index( ) {
+	public String method( ) {
 		return "viral";
+	}
+	@RequestMapping("/challenge")
+	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challenge";
 	}
 }
